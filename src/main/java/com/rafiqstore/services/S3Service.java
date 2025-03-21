3a9ce942155ebfe21cde0bc6a3b25857880f91db
Abu Bakar <siddique0153@gmail.com>
2025-03-21 23:59:10 +0600

    iamage resource method updated with cloude fire

--- src/main/java/com/rafiqstore/services/S3Service.java
+++ src/main/java/com/rafiqstore/services/S3Service.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 import java.util.UUID;
+
 @Service
 public class S3Service {
 
@@ -19,27 +19,24 @@ public class S3Service {
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
-    // Add the public R2.dev bucket URL as a configuration value
-    @Value("${cloud.r2.public-url}")
-    private String publicBucketUrl;
-
     public String uploadFile(MultipartFile file, String itemName) throws IOException {
         if (file == null || file.isEmpty()) {
             return null;
         }
 
+        // Generate a unique file name
         String fileName = itemName + "_" + UUID.randomUUID() + ".jpg";
 
-        // Upload the file to Cloudflare R2
+        // Upload the file to R2
         s3Client.putObject(PutObjectRequest.builder()
                         .bucket(bucketName)
                         .key(fileName)
                         .build(),
-                RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+                software.amazon.awssdk.core.sync.RequestBody.fromInputStream(
+                        file.getInputStream(), file.getSize())
+        );
 
-        // Return the publicly accessible file URL
-        return "https://" + bucketName + ".r2.dev/" + fileName;
+        // Return the file URL
+        return "https://" + bucketName + ".r2.cloudflarestorage.com/" + fileName;
     }
-
 }
-
