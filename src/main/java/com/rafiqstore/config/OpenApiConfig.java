8b0ecde62aaadbcebef16175547a7c45bd60ec70
Abu Bakar <siddique0153@gmail.com>
2025-08-25 10:37:34 +0600

    swagger configure

---
+++ src/main/java/com/rafiqstore/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.rafiqstore.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Student Management API")
+                        .description("API for managing students")
+                        .version("1.0.0"));
+    }
+}
